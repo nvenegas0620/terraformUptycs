719f3828a76e7bafbd888fb87f0f2df18991732d
Noel Isidro Venegas Vargas <noel.venegas@ibm.com>
2025-10-25 23:24:47 -0600

    Subir script Terraform con firewall, WAF, DDOS y OpenShift

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,26 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/hashicorp/azuread" {
+  version     = "2.53.1"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:7ZNdNGnUB6N6Z6St3COzRXFaghMEyYkZt7WyOCRKOqo=",
+    "zh:162916b037e5133f49298b0ffa3e7dcef7d76530a8ca738e7293373980f73c68",
+    "zh:1c3e89cf19118fc07d7b04257251fc9897e722c16e0a0df7b07fcd261f8c12e7",
+    "zh:492931cea4f30887ab5bca36a8556dfcb897288eddd44619c0217fc5da2d57e7",
+    "zh:4c895e450e18335ad8714cc6d3488fc1a78816ad2851a91b06cb2ef775dd7c66",
+    "zh:60d92fdaf7235574201f2d8f68f733ee00a822993b3fc95e6952e09e6ec76999",
+    "zh:67a169119efa41c1fb867ef1a8e79bf03472a2324384c36eb55370c817dcce42",
+    "zh:9dd4d5ed9233cf9329262200bc5a1aa60942b80dbc611e2ef4b09f47531b39b1",
+    "zh:a3c160e35b9e40fc1497b83c2f37a8e24565b05a1783c7733609f3695735c2a9",
+    "zh:a4a221da42b1f46e7c436c7145e5beaadfd9d03f3be6fd526d132c03f18a5979",
+    "zh:af0d3476a9702d2287e168e3baa670e64daab9c9b01c01e17025a5248f3e28e9",
+    "zh:e3579bff7894f3d36066b74ec324be6d28f56a42a387a2b8a0eabf33cbff86df",
+    "zh:f1749ee8ad972ae6424665aa9d2c0ece8c40c51d41ec2f38b863148cb437e865",
+  ]
+}
+
 provider "registry.terraform.io/hashicorp/azurerm" {
   version     = "4.49.0"
   constraints = "~> 4.11"
@@ -18,5 +38,24 @@ provider "registry.terraform.io/hashicorp/azurerm" {
     "zh:da617b754311f2eef250d748d9a75bc3c00e8e0843026bb8733acc314ec74c5f",
     "zh:f89b7db34cbf59cf67a043099e808f296a328000848f18cc8767cd19eb432f12",
     "zh:febe50653074bdd9a78e5a51a243c0fac6c12012c046877b7face253db42ddb8",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version = "3.2.4"
+  hashes = [
+    "h1:+Ag4hSb4qQjNtAS6gj2+gsGl7v0iB/Bif6zZZU8lXsw=",
+    "zh:59f6b52ab4ff35739647f9509ee6d93d7c032985d9f8c6237d1f8a59471bbbe2",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:795c897119ff082133150121d39ff26cb5f89a730a2c8c26f3a9c1abf81a9c43",
+    "zh:7b9c7b16f118fbc2b05a983817b8ce2f86df125857966ad356353baf4bff5c0a",
+    "zh:85e33ab43e0e1726e5f97a874b8e24820b6565ff8076523cc2922ba671492991",
+    "zh:9d32ac3619cfc93eb3c4f423492a8e0f79db05fec58e449dee9b2d5873d5f69f",
+    "zh:9e15c3c9dd8e0d1e3731841d44c34571b6c97f5b95e8296a45318b94e5287a6e",
+    "zh:b4c2ab35d1b7696c30b64bf2c0f3a62329107bd1a9121ce70683dec58af19615",
+    "zh:c43723e8cc65bcdf5e0c92581dcbbdcbdcf18b8d2037406a5f2033b1e22de442",
+    "zh:ceb5495d9c31bfb299d246ab333f08c7fb0d67a4f82681fbf47f2a21c3e11ab5",
+    "zh:e171026b3659305c558d9804062762d168f50ba02b88b231d20ec99578a6233f",
+    "zh:ed0fe2acdb61330b01841fa790be00ec6beaac91d41f311fb8254f74eb6a711f",
   ]
 }
--- main.tf
+++ main.tf
@@ -1,88 +1,252 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.11"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.48"
+    }
   }
 }
 
+############################################
+# Providers
+############################################
 provider "azurerm" {
   features {}
   subscription_id = "2944c97d-8d49-4931-a638-3535dde1dfd2"
 }
 
-# --------------------------
-# Basic parameters
-# --------------------------
-variable "location"          { default = "eastus" }
-variable "resource_group"    { default = "rg-secure-vms" }
-variable "vnet_cidr"         { default = "10.50.0.0/16" }
-variable "subnet_cidr"       { default = "10.50.1.0/24" }
-variable "vm_admin_username" { default = "azureuser" }
-variable "ssh_public_key"    { description = "Full content of your SSH public key (~/.ssh/id_rsa.pub)" }
+provider "azuread" {}
 
-# --------------------------
+############################################
+# Variables
+############################################
+variable "location"             { default = "eastus" }
+variable "resource_group"       { default = "rg-secure-vms" }
+variable "vnet_cidr"            { default = "10.50.0.0/16" }
+variable "subnet_cidr"          { default = "10.50.1.0/24" }
+variable "firewall_subnet_cidr" { default = "10.50.3.0/26" }
+variable "appgw_subnet_cidr"    { default = "10.50.4.0/27" }
+
+variable "vm_admin_username" { default = "azureuser" }
+
+variable "ssh_public_key" {
+  description = "Full content of your SSH public key"
+  type        = string
+}
+
+variable "enable_identity" {
+  type    = bool
+  default = false
+}
+
+variable "enable_aro" {
+  type    = bool
+  default = false
+}
+
+variable "aro_pull_secret_path" {
+  type    = string
+  default = ""
+}
+
+variable "aro_version" {
+  type    = string
+  default = "4.14.29"
+}
+
+############################################
 # Resource Group
-# --------------------------
+############################################
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = var.location
+  tags     = { owner = "noel", env = "dev" }
 }
 
-# --------------------------
-# Networking: VNet/Subnet + NAT Gateway for Internet egress
-# --------------------------
+############################################
+# Delay helper so Azure finishes propagating RG
+############################################
+resource "null_resource" "wait_for_rg" {
+  provisioner "local-exec" {
+    # PowerShell sleep ~10 seconds to let ARM replicate RG metadata
+    command = "powershell -Command Start-Sleep -Seconds 10"
+  }
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+############################################
+# DDoS Protection Plan
+############################################
+resource "azurerm_network_ddos_protection_plan" "ddos" {
+  name                = "ddos-plan"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  depends_on = [
+    null_resource.wait_for_rg
+  ]
+}
+
+############################################
+# VNet + Subnets
+############################################
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-core"
+  location            = var.location
+  resource_group_name = var.resource_group
   address_space       = [var.vnet_cidr]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+
+  ddos_protection_plan {
+    id     = azurerm_network_ddos_protection_plan.ddos.id
+    enable = true
+  }
+
+  depends_on = [
+    null_resource.wait_for_rg
+  ]
 }
 
 resource "azurerm_subnet" "subnet_app" {
   name                 = "snet-app"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = var.resource_group
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_cidr]
+
+  depends_on = [
+    null_resource.wait_for_rg,
+    azurerm_virtual_network.vnet
+  ]
 }
 
-# Public IP (Standard) for NAT Gateway
-resource "azurerm_public_ip" "nat_pip" {
-  name                = "pip-nat-egress"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_subnet" "firewall_subnet" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.firewall_subnet_cidr]
+
+  depends_on = [
+    null_resource.wait_for_rg,
+    azurerm_virtual_network.vnet
+  ]
+}
+
+resource "azurerm_subnet" "appgw_subnet" {
+  name                 = "snet-appgw"
+  resource_group_name  = var.resource_group
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.appgw_subnet_cidr]
+
+  depends_on = [
+    null_resource.wait_for_rg,
+    azurerm_virtual_network.vnet
+  ]
+}
+
+############################################
+# Azure Firewall + egress rules
+############################################
+resource "azurerm_public_ip" "fw_pip" {
+  name                = "pip-fw"
+  location            = var.location
+  resource_group_name = var.resource_group
   allocation_method   = "Static"
   sku                 = "Standard"
+
+  depends_on = [
+    null_resource.wait_for_rg
+  ]
 }
 
-# NAT Gateway to provide outbound Internet access
-resource "azurerm_nat_gateway" "nat" {
-  name                = "ngw-egress"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Standard"
+resource "azurerm_firewall" "fw" {
+  name                = "azurefw"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  sku_name = "AZFW_VNet"
+  sku_tier = "Standard"
+
+  ip_configuration {
+    name                 = "fw-ipcfg"
+    subnet_id            = azurerm_subnet.firewall_subnet.id
+    public_ip_address_id = azurerm_public_ip.fw_pip.id
+  }
+
+  threat_intel_mode = "Alert"
+
+  depends_on = [
+    null_resource.wait_for_rg,
+    azurerm_subnet.firewall_subnet,
+    azurerm_public_ip.fw_pip
+  ]
 }
 
-resource "azurerm_nat_gateway_public_ip_association" "nat_assoc_pip" {
-  nat_gateway_id       = azurerm_nat_gateway.nat.id
-  public_ip_address_id = azurerm_public_ip.nat_pip.id
+resource "azurerm_firewall_network_rule_collection" "allow_outbound" {
+  name                = "Allow-Outbound-Web"
+  azure_firewall_name = azurerm_firewall.fw.name
+  resource_group_name = var.resource_group
+  priority            = 100
+  action              = "Allow"
+
+  rule {
+    name                  = "Allow-HTTP-HTTPS"
+    protocols             = ["TCP"]
+    source_addresses      = ["10.50.0.0/16"]
+    destination_addresses = ["0.0.0.0/0"]
+    destination_ports     = ["80", "443"]
+  }
+
+  depends_on = [
+    azurerm_firewall.fw
+  ]
 }
 
-resource "azurerm_subnet_nat_gateway_association" "snet_nat_assoc" {
-  subnet_id      = azurerm_subnet.subnet_app.id
-  nat_gateway_id = azurerm_nat_gateway.nat.id
+############################################
+# Route table to force egress via Firewall
+############################################
+resource "azurerm_route_table" "rt_app" {
+  name                = "rt-snet-app-egress-fw"
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  depends_on = [
+    null_resource.wait_for_rg
+  ]
 }
 
-# Optional: Basic NSG (Network Security Group)
-# Azure allows outbound traffic by default, but we define explicit rules for clarity.
+resource "azurerm_route" "default_to_fw" {
+  name                   = "default-to-fw"
+  resource_group_name    = var.resource_group
+  route_table_name       = azurerm_route_table.rt_app.name
+  address_prefix         = "0.0.0.0/0"
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = azurerm_firewall.fw.ip_configuration[0].private_ip_address
+
+  depends_on = [
+    azurerm_firewall.fw,
+    azurerm_route_table.rt_app
+  ]
+}
+
+# NOT managing the association here anymore because it caused "already exists" issues
+# If you want Terraform to own it later, we'll import first, then add back.
+
+############################################
+# NSG for workload subnet (deny inbound / allow outbound)
+############################################
 resource "azurerm_network_security_group" "nsg_app" {
   name                = "nsg-app"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group
 
-  # Deny all inbound traffic
   security_rule {
     name                       = "deny-all-inbound"
     priority                   = 100
@@ -95,7 +259,6 @@ resource "azurerm_network_security_group" "nsg_app" {
     destination_address_prefix = "*"
   }
 
-  # Allow all outbound traffic
   security_rule {
     name                       = "allow-all-outbound"
     priority                   = 200
@@ -107,55 +270,157 @@ resource "azurerm_network_security_group" "nsg_app" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  depends_on = [
+    null_resource.wait_for_rg
+  ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "snet_nsg" {
   subnet_id                 = azurerm_subnet.subnet_app.id
   network_security_group_id = azurerm_network_security_group.nsg_app.id
+
+  depends_on = [
+    azurerm_subnet.subnet_app,
+    azurerm_network_security_group.nsg_app
+  ]
 }
 
-# --------------------------
-# NICs (no public IPs)
-# --------------------------
+############################################
+# Application Gateway (WAF_v2)
+############################################
+resource "azurerm_public_ip" "appgw_pip" {
+  name                = "pip-appgw"
+  location            = var.location
+  resource_group_name = var.resource_group
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  depends_on = [
+    null_resource.wait_for_rg
+  ]
+}
+
+resource "azurerm_application_gateway" "appgw" {
+  name                = "appgw-waf"
+  resource_group_name = var.resource_group
+  location            = var.location
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 1
+  }
+
+  gateway_ip_configuration {
+    name      = "appgw-ipcfg"
+    subnet_id = azurerm_subnet.appgw_subnet.id
+  }
+
+  frontend_ip_configuration {
+    name                 = "publicFrontend"
+    public_ip_address_id = azurerm_public_ip.appgw_pip.id
+  }
+
+  frontend_port {
+    name = "http-port"
+    port = 80
+  }
+
+  backend_address_pool {
+    name         = "backendPool"
+    ip_addresses = [
+      azurerm_network_interface.nic_vm1.ip_configuration[0].private_ip_address
+    ]
+  }
+
+  backend_http_settings {
+    name                  = "bhs-http"
+    port                  = 80
+    protocol              = "Http"
+    cookie_based_affinity = "Disabled"
+    request_timeout       = 30
+  }
+
+  http_listener {
+    name                           = "http-listener"
+    frontend_ip_configuration_name = "publicFrontend"
+    frontend_port_name             = "http-port"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "rule1"
+    rule_type                  = "Basic"
+    http_listener_name         = "http-listener"
+    backend_address_pool_name  = "backendPool"
+    backend_http_settings_name = "bhs-http"
+    priority                   = 100
+  }
+
+  waf_configuration {
+    enabled          = true
+    firewall_mode    = "Detection"
+    rule_set_type    = "OWASP"
+    rule_set_version = "3.2"
+  }
+
+  depends_on = [
+    null_resource.wait_for_rg,
+    azurerm_network_interface.nic_vm1,
+    azurerm_subnet.appgw_subnet,
+    azurerm_public_ip.appgw_pip
+  ]
+}
+
+############################################
+# NICs + VMs
+############################################
 resource "azurerm_network_interface" "nic_vm1" {
   name                = "nic-vm1"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.subnet_app.id
     private_ip_address_allocation = "Dynamic"
   }
+
+  depends_on = [
+    null_resource.wait_for_rg,
+    azurerm_subnet.subnet_app
+  ]
 }
 
 resource "azurerm_network_interface" "nic_vm2" {
   name                = "nic-vm2"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.subnet_app.id
     private_ip_address_allocation = "Dynamic"
   }
+
+  depends_on = [
+    null_resource.wait_for_rg,
+    azurerm_subnet.subnet_app
+  ]
 }
 
-# --------------------------
-# Availability Set (recommended for multiple VMs)
-# --------------------------
 resource "azurerm_availability_set" "aset" {
-  name                         = "aset-app"
-  location                     = azurerm_resource_group.rg.location
-  resource_group_name           = azurerm_resource_group.rg.name
-  platform_fault_domain_count   = 2
-  platform_update_domain_count  = 5
-  managed = true
+  name                = "aset-app"
+  location            = var.location
+  resource_group_name = var.resource_group
+  managed             = true
+
+  depends_on = [
+    null_resource.wait_for_rg
+  ]
 }
 
-# --------------------------
-# Two Linux Virtual Machines (Ubuntu LTS)
-# --------------------------
 locals {
   vm_size = "Standard_B2s"
   image = {
@@ -167,13 +432,13 @@ locals {
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-  name                = "vm1-app"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = local.vm_size
-  admin_username      = var.vm_admin_username
+  name                  = "vm1-app"
+  location              = var.location
+  resource_group_name   = var.resource_group
+  size                  = local.vm_size
+  admin_username        = var.vm_admin_username
   network_interface_ids = [azurerm_network_interface.nic_vm1.id]
-  availability_set_id = azurerm_availability_set.aset.id
+  availability_set_id   = azurerm_availability_set.aset.id
 
   admin_ssh_key {
     username   = var.vm_admin_username
@@ -194,16 +459,21 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 
   disable_password_authentication = true
+
+  depends_on = [
+    azurerm_network_interface.nic_vm1,
+    azurerm_availability_set.aset
+  ]
 }
 
 resource "azurerm_linux_virtual_machine" "vm2" {
-  name                = "vm2-app"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = local.vm_size
-  admin_username      = var.vm_admin_username
+  name                  = "vm2-app"
+  location              = var.location
+  resource_group_name   = var.resource_group
+  size                  = local.vm_size
+  admin_username        = var.vm_admin_username
   network_interface_ids = [azurerm_network_interface.nic_vm2.id]
-  availability_set_id = azurerm_availability_set.aset.id
+  availability_set_id   = azurerm_availability_set.aset.id
 
   admin_ssh_key {
     username   = var.vm_admin_username
@@ -224,22 +494,107 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   }
 
   disable_password_authentication = true
-}
 
-# --------------------------
-# Outputs
-# --------------------------
-output "subnet_id" {
-  value = azurerm_subnet.subnet_app.id
-}
-
-output "nat_gateway_public_ip" {
-  value = azurerm_public_ip.nat_pip.ip_address
-}
-
-output "vm_private_ips" {
-  value = [
-    azurerm_network_interface.nic_vm1.ip_configuration[0].private_ip_address,
-    azurerm_network_interface.nic_vm2.ip_configuration[0].private_ip_address
+  depends_on = [
+    azurerm_network_interface.nic_vm2,
+    azurerm_availability_set.aset
   ]
+}
+
+############################################
+# Subscription data (for future ARO/identity use)
+############################################
+data "azurerm_subscription" "primary" {}
+
+############################################
+# Conditional Entra ID + ARO
+############################################
+resource "azuread_application" "aro_app" {
+  count            = var.enable_identity ? 1 : 0
+  display_name     = "tf-aro-app"
+  sign_in_audience = "AzureADMyOrg"
+}
+
+resource "azuread_service_principal" "aro_sp" {
+  count     = var.enable_identity ? 1 : 0
+  client_id = var.enable_identity ? azuread_application.aro_app[0].client_id : null
+}
+
+resource "azuread_application_password" "aro_app_secret" {
+  count                 = var.enable_identity ? 1 : 0
+  application_object_id = var.enable_identity ? azuread_application.aro_app[0].object_id : null
+  display_name          = "tf-aro-secret"
+  end_date_relative     = "8760h"
+}
+
+resource "azurerm_role_assignment" "aro_sp_contrib" {
+  count                = var.enable_identity ? 1 : 0
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Contributor"
+  principal_id         = var.enable_identity ? azuread_service_principal.aro_sp[0].object_id : null
+}
+
+resource "azurerm_redhat_openshift_cluster" "aro" {
+  count               = var.enable_aro ? 1 : 0
+  name                = "aro-cluster"
+  resource_group_name = var.resource_group
+  location            = var.location
+
+  cluster_profile {
+    version     = var.aro_version
+    pull_secret = file(var.aro_pull_secret_path)
+    domain      = "aro.example.internal"
+  }
+
+  service_principal {
+    client_id     = azuread_application.aro_app[0].client_id
+    client_secret = azuread_application_password.aro_app_secret[0].value
+  }
+
+  main_profile {
+    vm_size   = "Standard_D4s_v3"
+    subnet_id = azurerm_subnet.subnet_app.id
+  }
+
+  worker_profile {
+    vm_size       = "Standard_D4s_v3"
+    subnet_id     = azurerm_subnet.subnet_app.id
+    node_count    = 3
+    disk_size_gb  = 128
+  }
+
+  api_server_profile {
+    visibility = "Public"
+  }
+
+  ingress_profile {
+    visibility = "Public"
+  }
+
+  network_profile {
+    pod_cidr     = "10.128.0.0/14"
+    service_cidr = "172.30.0.0/16"
+  }
+
+  tags = { env = "dev" }
+
+  depends_on = [
+    azurerm_role_assignment.aro_sp_contrib,
+    null_resource.wait_for_rg
+  ]
+}
+
+############################################
+# Outputs
+############################################
+output "firewall_public_ip" {
+  value = azurerm_public_ip.fw_pip.ip_address
+}
+
+output "firewall_private_ip" {
+  value = azurerm_firewall.fw.ip_configuration[0].private_ip_address
+}
+
+output "appgw_public_ip" {
+  value = azurerm_public_ip.appgw_pip.ip_address
 }
